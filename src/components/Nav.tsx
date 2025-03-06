0c7054eeca73f9fc79a881cac7f9be07433f1bff
JossieHaven <jossiehaven@gmail.com>
2025-03-05 21:32:39 -0600

    Committing code for Nav and Candidate.interfaces

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,24 @@
+import { NavLink } from "react-router-dom";
+import "../index.css"
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav className="nav">
+    <ul>
+      <li className="nav-item">
+        <NavLink to="/" className="nav-link">
+          Candidate Search
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink to="/saved" className="nav-link">
+          Saved Candidates
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+  );
 };
 
 export default Nav;
